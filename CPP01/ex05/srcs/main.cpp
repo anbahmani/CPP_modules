5149245c02539f5f4f27c8ff864c485c298c16a2
abahmani <abahmani@student.42.fr>
2022-11-19 22:23:42 +0100

    fixed cpp01 ex04

--- CPP01/ex05/srcs/main.cpp
+++ CPP01/ex05/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: abahmani <abahmani@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/29 15:53:25 by abahmani          #+#    #+#             */
-/*   Updated: 2022/09/29 16:29:02 by abahmani         ###   ########.fr       */
+/*   Updated: 2022/11/19 22:01:10 by abahmani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,37 @@
 int main(){
 	Harl harl;
 
+	std::cout << "Harl prints : ";
 	harl.complain("DEBUG");
+	std::cout << "Harl does not print." << std::endl;;
+	harl.complain("DEBUGS");
+	std::cout << "Harl does not print." << std::endl;;
+	harl.complain("DEBU");
+	std::cout << "Harl prints : ";
 	harl.complain("INFO");
+	std::cout << "Harl does not print." << std::endl;;
+	harl.complain("INFO!");
+	std::cout << "Harl does not print." << std::endl;;
+	harl.complain("INF");
+	std::cout << "Harl prints : ";
 	harl.complain("WARNING");
+	std::cout << "Harl does not print." << std::endl;;
+	harl.complain("WARNINGS");
+	std::cout << "Harl does not print." << std::endl;;
+	harl.complain("WARNI");
+	std::cout << "Harl prints : ";
 	harl.complain("ERROR");
+	std::cout << "Harl does not print." << std::endl;;
+	harl.complain("ERRORA");
+	std::cout << "Harl does not print." << std::endl;;
+	harl.complain("ERRO");
+	std::cout << "Harl does not print." << std::endl;;
 	harl.complain("TEST");
+	std::cout << "Harl does not print." << std::endl;;
+	harl.complain("");
+	std::cout << "Harl does not print." << std::endl;;
+	harl.complain("42");
+	std::cout << "Harl does not print." << std::endl;;
+	harl.complain("$$$");
 	return (0);
 }
